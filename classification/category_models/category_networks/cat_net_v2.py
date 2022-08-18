fd0b1d387585569de953545454882815c76ea1dc
Josephine Selig <js12ziwi@studserv.uni-leipzig.de>
2022-08-18 22:18:21 +0200

    base NN - not yet fitted

--- classification/category_models/category_networks/cat_net_v2.py
+++ classification/category_models/category_networks/cat_net_v2.py
@@ -1,5 +1,10 @@
 import random
 from typing import List
+from nltk.stem.lancaster import LancasterStemmer
+import nltk
+import numpy as np
+
+import time
 
 from .base_category_network import BaseCategoryNetwork
 from ...preprocessing import Category
@@ -13,6 +18,8 @@ class CategoryNetworkV0(BaseCategoryNetwork):
     def predict(self, web_ids: List[str]) -> List[Category]:
         return [random.choice(Category) for _ in web_ids]
 
-    def train(self, web_ids: List[str]) -> None:
-        pass
 
+
+    def train(self, web_ids: List[str]) -> None:
+
+        pass
