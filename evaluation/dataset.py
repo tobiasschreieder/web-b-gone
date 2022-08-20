06438e9cc31bba40390d8723fc3279b556757d96
Tobias Schreieder <fp83rusi@studserv.uni-leipzig.de>
2022-08-20 18:51:23 +0200

    performance boost for eda and small changes

--- evaluation/dataset.py
+++ evaluation/dataset.py
@@ -1,26 +1,41 @@
+import copy
 from typing import List, Dict
 
 from classification.preprocessing import GroundTruth, Website
 
 
-def exploratory_data_analysis(max_size: int = -1):
+def exploratory_data_analysis(max_size: int = -1, name: str = "", path: str = "working/", data=None):
     """
     Run exploratory data analysis
+    :param data: List with dataset that contains extraction results of a model
+    :param path: String with path to save MD-File
+    :param name: String with name of considered dataset
     :param max_size: Size of sample which should be used, -1 -> all data will be used
     """
-    web_ids: List[str] = Website.get_website_ids(max_size=max_size, rdm_sample=True, seed='eval_class')
-    ground_truth = [GroundTruth.load(web_id).attributes for web_id in web_ids]
+    # Load dataset
+    if not data:
+        web_ids: List[str] = Website.get_website_ids(max_size=max_size, rdm_sample=True, seed='eval_class')
+        ground_truth = [GroundTruth.load(web_id).attributes for web_id in web_ids]
+    else:
+        ground_truth = data
 
-    length_dict = average_length_ground_truth(ground_truth=ground_truth)
-    token_dict = average_tokens_ground_truth(ground_truth=ground_truth)
-    solution_dict = average_solutions_ground_truth(ground_truth=ground_truth)
-    missing_dict = average_missing_ground_truth(ground_truth=ground_truth)
+    # Create datastructures to save later calculated metrics
+    dictionary = get_dictionary(ground_truth=ground_truth)
+    count_category_ids = count_websites_per_category(ground_truth=ground_truth)
 
+    # Calculate metrics
+    length_dict = average_length_ground_truth(ground_truth=ground_truth, dictionary=copy.deepcopy(dictionary),
+                                              count_category_ids=count_category_ids)
+    token_dict = average_tokens_ground_truth(ground_truth=ground_truth, dictionary=copy.deepcopy(dictionary),
+                                             count_category_ids=count_category_ids)
+    solution_dict = average_solutions_ground_truth(ground_truth=ground_truth, dictionary=copy.deepcopy(dictionary),
+                                                   count_category_ids=count_category_ids)
+    missing_dict = average_missing_ground_truth(ground_truth=ground_truth, dictionary=copy.deepcopy(dictionary),
+                                                count_category_ids=count_category_ids)
+
+    # Save metrics as MD-File
     eda = [length_dict, token_dict, solution_dict, missing_dict]
-
-    create_eda_md_table(eda=eda)
-
-    # ToDo: further processing of the results deepcopy
+    create_eda_md_table(eda=eda, name=name, path=path)
 
 
 def get_dictionary(ground_truth: List[Dict[str, List[str]]]) -> Dict[str, Dict[str, float]]:
@@ -58,14 +73,17 @@ def count_websites_per_category(ground_truth: List[Dict[str, List[str]]]) -> Dic
     return count_category_ids
 
 
-def average_length_ground_truth(ground_truth: List[Dict[str, List[str]]]) -> Dict[str, Dict[str, float]]:
+def average_length_ground_truth(ground_truth: List[Dict[str, List[str]]], dictionary: Dict[str, Dict[str, float]],
+                                count_category_ids: Dict[str, int]) -> Dict[str, Dict[str, float]]:
     """
     Calculate average number of chars for ground-truth string per category and attribute
+    :param count_category_ids: Dictionary with number of websites per category
+    :param dictionary: Datastructure that is used to save calculated results
     :param ground_truth: List with Ground-truth elements
     :return: Results saved in dictionary structure
     """
     ground_truth = ground_truth
-    length_dict = get_dictionary(ground_truth=ground_truth)
+    length_dict = dictionary
 
     for gt in ground_truth:
         for attribute, value in gt.items():
@@ -80,8 +98,6 @@ def average_length_ground_truth(ground_truth: List[Dict[str, List[str]]]) -> Dic
                     length_value = 0
                 length_dict[category][attribute] += length_value
 
-    count_category_ids = count_websites_per_category(ground_truth=ground_truth)
-
     for category in length_dict:
         length = count_category_ids[category]
         for attribute in length_dict[category]:
@@ -90,14 +106,17 @@ def average_length_ground_truth(ground_truth: List[Dict[str, List[str]]]) -> Dic
     return length_dict
 
 
-def average_tokens_ground_truth(ground_truth: List[Dict[str, List[str]]]) -> Dict[str, Dict[str, float]]:
+def average_tokens_ground_truth(ground_truth: List[Dict[str, List[str]]], dictionary: Dict[str, Dict[str, float]],
+                                count_category_ids: Dict[str, int]) -> Dict[str, Dict[str, float]]:
     """
     Calculate average number of tokens (words) for ground-truth string per category and attribute
+    :param count_category_ids: Dictionary with number of websites per category
+    :param dictionary: Datastructure that is used to save calculated results
     :param ground_truth: List with Ground-truth elements
     :return: Results saved in dictionary structure
     """
     ground_truth = ground_truth
-    token_dict = get_dictionary(ground_truth=ground_truth)
+    token_dict = dictionary
 
     for gt in ground_truth:
         for attribute, value in gt.items():
@@ -112,8 +131,6 @@ def average_tokens_ground_truth(ground_truth: List[Dict[str, List[str]]]) -> Dic
                     count_tokens = 0
                 token_dict[category][attribute] += count_tokens
 
-    count_category_ids = count_websites_per_category(ground_truth=ground_truth)
-
     for category in token_dict:
         length = count_category_ids[category]
         for attribute in token_dict[category]:
@@ -122,22 +139,23 @@ def average_tokens_ground_truth(ground_truth: List[Dict[str, List[str]]]) -> Dic
     return token_dict
 
 
-def average_solutions_ground_truth(ground_truth: List[Dict[str, List[str]]]) -> Dict[str, Dict[str, float]]:
+def average_solutions_ground_truth(ground_truth: List[Dict[str, List[str]]], dictionary: Dict[str, Dict[str, float]],
+                                   count_category_ids: Dict[str, int]) -> Dict[str, Dict[str, float]]:
     """
     Calculate average number of different solutions in ground-truth per category and attribute
+    :param count_category_ids: Dictionary with number of websites per category
+    :param dictionary: Datastructure that is used to save calculated results
     :param ground_truth: List with Ground-truth elements
     :return: Results saved in dictionary structure
     """
     ground_truth = ground_truth
-    solution_dict = get_dictionary(ground_truth=ground_truth)
+    solution_dict = dictionary
 
     for gt in ground_truth:
         for attribute, value in gt.items():
             category = gt["category"]
             if attribute != "category":
                 solution_dict[category][attribute] += len(value)
-
-    count_category_ids = count_websites_per_category(ground_truth=ground_truth)
 
     for category in solution_dict:
         length = count_category_ids[category]
@@ -147,14 +165,17 @@ def average_solutions_ground_truth(ground_truth: List[Dict[str, List[str]]]) -> 
     return solution_dict
 
 
-def average_missing_ground_truth(ground_truth: List[Dict[str, List[str]]]) -> Dict[str, Dict[str, float]]:
+def average_missing_ground_truth(ground_truth: List[Dict[str, List[str]]], dictionary: Dict[str, Dict[str, float]],
+                                 count_category_ids: Dict[str, int]) -> Dict[str, Dict[str, float]]:
     """
     Calculate average number of missing ground-truth per category and attribute
+    :param count_category_ids: Dictionary with number of websites per category
+    :param dictionary: Datastructure that is used to save calculated results
     :param ground_truth: List with Ground-truth elements
     :return: Results saved in dictionary structure
     """
     ground_truth = ground_truth
-    missing_dict = get_dictionary(ground_truth=ground_truth)
+    missing_dict = dictionary
 
     for gt in ground_truth:
         for attribute, value in gt.items():
@@ -171,8 +192,6 @@ def average_missing_ground_truth(ground_truth: List[Dict[str, List[str]]]) -> Di
                 if missing_value:
                     missing_dict[category][attribute] += 1
 
-    count_category_ids = count_websites_per_category(ground_truth=ground_truth)
-
     for category in missing_dict:
         length = count_category_ids[category]
         for attribute in missing_dict[category]:
@@ -181,15 +200,28 @@ def average_missing_ground_truth(ground_truth: List[Dict[str, List[str]]]) -> Di
     return missing_dict
 
 
-def create_eda_md_table(eda: List[Dict[str, Dict[str, float]]]):
+def create_eda_md_table(eda: List[Dict[str, Dict[str, float]]], name: str, path: str):
     """
     Create Markdown File with Table of EDA in /working
+    :param path: String with path to save MD-File
+    :param name: String with name of considered dataset
     :param eda: List with all calculated exploratory data analysis results
     """
+    # Header
     text = list()
     text.append("# Exploratory Data Analysis")
+
+    # Description of measures
+    text.append("**Average Length** = Average number of characters / length of solution calculated for each attribute "
+                "and category.\n")
+    text.append("**Average Tokens** = Average number of tokens (words) calculated for each attribute and category.\n")
+    text.append("**Average Solutions** = Average number of existing solutions calculated for each attribute and "
+                "category.\n")
+    text.append("**Average Missing** = Average proportion of missing solutions calculated for each attribute and "
+                "category.\n")
     text.append("\n")
 
+    # Create MD-Tables for each category
     for category in eda[0]:
         text.append("## " + "Category: " + category.upper() + "\n")
         text.append("| Attribute | Average Length | Average Tokens | Average Solutions | Average Missing |")
@@ -205,7 +237,14 @@ def create_eda_md_table(eda: List[Dict[str, Dict[str, float]]]):
 
         text.append("\n")
 
-    with open('working/exploratory_data_analysis.md', 'w') as f:
+    # Specify path and file-name
+    save_name = "eda"
+    if name != "":
+        save_name += "_" + name
+    save_name = path + save_name + ".md"
+
+    # Save MD-File
+    with open(save_name, 'w') as f:
         for item in text:
             f.write("%s\n" % item)
 
