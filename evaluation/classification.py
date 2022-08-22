dfa2290119e199121bae89996d28a05b1a923ea8
Tobias Schreieder <fp83rusi@studserv.uni-leipzig.de>
2022-08-22 20:27:51 +0200

    add split criteria

--- evaluation/classification.py
+++ evaluation/classification.py
@@ -1,4 +1,4 @@
-from typing import List, Dict, Type
+from typing import List, Dict, Type, Tuple
 
 import numpy as np
 from sklearn.metrics import confusion_matrix, recall_score, precision_score, f1_score
@@ -11,20 +11,19 @@ AVERAGE = "macro"  # determines the type of averaging performed on the data, cho
 
 def evaluate_classification(model_cls_classification: Type[BaseCategoryModel],
                             train_test_split: float,
-                            max_size: int = -1, **model_kwargs) -> Dict[str, float]:
+                            max_size: int = -1, **model_kwargs) -> Dict[str, Dict[str, float]]:
     """
     Evaluate a given classification model
     :param model_cls_classification: Classification model which should be used
-    :param train_test_split: Specify proportion of train data
+    :param train_test_split: Specify proportion of train data [0; 1]
     :param max_size: Size of sample which should be used, -1 -> all data will be used
     :param model_kwargs:
     :return: Dictionary with calculated metric scores
     """
     # Load and split data
-    web_ids: List[str] = Website.get_website_ids(max_size=max_size, rdm_sample=True, seed='eval_class')
-    split_index = int(len(web_ids) * train_test_split)
-    train_ids = web_ids[:split_index]
-    test_ids = web_ids[split_index:]
+    train_ids: List[str]
+    test_ids: List[str]
+    train_ids, test_ids = split_data(train_test_split=train_test_split, split_type="website", max_size=max_size)
 
     # Classification
     model_classification: BaseCategoryModel
@@ -35,10 +34,65 @@ def evaluate_classification(model_cls_classification: Type[BaseCategoryModel],
     else:
         model_classification = model_cls_classification(**model_kwargs)
 
-    results_classification = classification_metrics(model_classification.classification(web_ids=test_ids),
-                                                    [GroundTruth.load(web_id).category for web_id in test_ids])
+    # out of sample prediction
+    if len(test_ids) != 0:
+        results_classification_test = classification_metrics(model_classification.classification(web_ids=test_ids),
+                                                        [GroundTruth.load(web_id).category for web_id in test_ids])
+    else:
+        results_classification_test = {"recall": None, "precision": None, "f1": None}
 
-    return results_classification
+    # in sample prediction
+    if len(train_ids) != 0:
+        results_classification_train = classification_metrics(model_classification.classification(web_ids=train_ids),
+                                                        [GroundTruth.load(web_id).category for web_id in train_ids])
+    else:
+        results_classification_train = {"recall": None, "precision": None, "f1": None}
+
+    results = {"out of sample": results_classification_test, "in sample": results_classification_train}
+
+    return results
+
+
+def split_data(train_test_split: float, split_type: str, max_size: int = -1,
+               seed: str = "eval_class") -> Tuple[List[str], List[str]]:
+    """
+    Method to split dataset with defined split-type
+    :param train_test_split: Specify proportion of train data [0; 1]
+    :param split_type: String to define Split-Type, Choose between "website" and "domain"
+    :param max_size: Size of sample which should be used, -1 -> all data will be used
+    :param seed: String with seed
+    :return: Tuple with train-ids and test-ids
+    """
+    train_ids = list()
+    test_ids = list()
+
+    if split_type == "website":
+        web_ids: List[str] = Website.get_website_ids(max_size=max_size, rdm_sample=True, seed=seed)
+
+        split_index = int(len(web_ids) * train_test_split)
+        train_ids = web_ids[:split_index]
+        test_ids = web_ids[split_index:]
+
+    elif split_type == "domain":
+        categories = [cat for cat in Category]
+
+        cat_size = int(max_size / len(categories))
+        for cat in categories:
+            domains = Website.get_all_domains(category=cat)
+            split_index = int(len(domains) * train_test_split)
+            train_domains = domains[:split_index]
+            test_domains = domains[split_index:]
+
+            train_ids += Website.get_website_ids(max_size=int(cat_size*train_test_split), rdm_sample=True, seed=seed, categories=cat,
+                                                 domains=train_domains)
+
+            test_ids += Website.get_website_ids(max_size=int(cat_size*(1-train_test_split)), rdm_sample=True, seed=seed, categories=cat,
+                                                domains=test_domains)
+
+    else:
+        return [], []
+
+    return train_ids, test_ids
 
 
 def format_data_classification(data: List[Category]) -> List[str]:
--- evaluation/extraction.py
+++ evaluation/extraction.py
@@ -1,4 +1,4 @@
-from typing import List, Dict, Type
+from typing import List, Dict, Type, Tuple
 
 from classification.preprocessing import Category, GroundTruth, Website
 from evaluation import comparison, text_preprocessing
@@ -8,7 +8,7 @@ from extraction.extraction_models import BaseExtractionModel
 def evaluate_extraction(model_cls_extraction: Type[BaseExtractionModel],
                         category: Category,
                         train_test_split: float,
-                        max_size: int = -1, **model_kwargs) -> Dict[str, float]:
+                        max_size: int = -1, **model_kwargs) -> Dict[str, Dict[str, float]]:
     """
     Evaluate a given extraction model
     :param model_cls_extraction: Extraction model which should be used
@@ -19,21 +19,72 @@ def evaluate_extraction(model_cls_extraction: Type[BaseExtractionModel],
     :return: Dictionary with calculated metric scores
     """
     # Load and split data
-    web_ids: List[str] = Website.get_website_ids(categories=category, max_size=max_size, rdm_sample=True,
-                                                 seed='eval_class')
-    split_index = int(len(web_ids)*train_test_split)
-    train_ids = web_ids[:split_index]
-    test_ids = web_ids[split_index:]
+    train_ids: List[str]
+    test_ids: List[str]
+    train_ids, test_ids = split_data(category=category, train_test_split=train_test_split, split_type="website",
+                                     max_size=max_size)
 
     # Extraction
     model_extraction: BaseExtractionModel
     model_extraction = model_cls_extraction(category, **model_kwargs)
     model_extraction.train(train_ids)
 
-    results_extraction = extraction_metrics(model_extraction.extract(web_ids=test_ids),
-                                            [GroundTruth.load(web_id).attributes for web_id in test_ids])
+    # out of sample prediction
+    if len(test_ids) != 0:
+        results_extraction_test = extraction_metrics(model_extraction.extract(web_ids=test_ids),
+                                                [GroundTruth.load(web_id).attributes for web_id in test_ids])
+    else:
+        results_extraction_test = {"exact_match": None, "f1": None}
 
-    return results_extraction
+    # in sample prediction
+    if len(train_ids) != 0:
+        results_extraction_train = extraction_metrics(model_extraction.extract(web_ids=train_ids),
+                                                     [GroundTruth.load(web_id).attributes for web_id in train_ids])
+    else:
+        results_extraction_train = {"exact_match": None, "f1": None}
+
+    results = {"out of sample": results_extraction_test, "in sample": results_extraction_train}
+
+    return results
+
+
+def split_data(category: Category, train_test_split: float, split_type: str, max_size: int = -1,
+               seed: str = "eval_class") -> Tuple[List[str], List[str]]:
+    """
+    Method to split dataset with defined split-type
+    :param category: Category that should be considered
+    :param train_test_split: Specify proportion of train data [0; 1]
+    :param split_type: String to define Split-Type, Choose between "website" and "domain"
+    :param max_size: Size of sample which should be used, -1 -> all data will be used
+    :param seed: String with seed
+    :return: Tuple with train-ids and test-ids
+    """
+    train_ids = list()
+    test_ids = list()
+
+    if split_type == "website":
+        web_ids: List[str] = Website.get_website_ids(categories=category, rdm_sample=True, seed=seed)
+
+        split_index = int(len(web_ids) * train_test_split)
+        train_ids = web_ids[:split_index]
+        test_ids = web_ids[split_index:]
+
+    elif split_type == "domain":
+        domains = Website.get_all_domains(category=category)
+        split_index = int(len(domains) * train_test_split)
+        train_domains = domains[:split_index]
+        test_domains = domains[split_index:]
+
+        train_ids += Website.get_website_ids(max_size=int(max_size * train_test_split), rdm_sample=True, seed=seed,
+                                             categories=category, domains=train_domains)
+
+        test_ids += Website.get_website_ids(max_size=int(max_size * (1 - train_test_split)), rdm_sample=True,
+                                            seed=seed, categories=category, domains=test_domains)
+
+    else:
+        return [], []
+
+    return train_ids, test_ids
 
 
 def format_data_extraction(data: List[Dict[str, List[str]]]) -> List[List[str]]:
