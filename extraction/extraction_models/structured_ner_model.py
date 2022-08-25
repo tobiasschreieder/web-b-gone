1583071d35f89e84a4af045742d3a443878272ef
Lorenz <lorenz.heinemann@web.de>
2022-08-25 17:00:08 +0200

    implpemented 3rd model (combination of NER and Structure)

--- extraction/extraction_models/structured_ner_model.py
+++ extraction/extraction_models/structured_ner_model.py
@@ -8,6 +8,7 @@ from .base_model import BaseExtractionModel
 from .. import nerHelper
 from ..structure_helper_v2 import StructuredTemplate
 from .extraction_networks.base_extraction_network import ExtractionNetwork
+from evaluation import text_preprocessing
 
 cfg = Config.get()
 
@@ -24,9 +25,7 @@ class CombinedExtractionModel(BaseExtractionModel):
     def __init__(self, category: Category, ner_name: str, struc_name: str):
         super().__init__(category)
         self.ner_name = ner_name
-        self.ner_name = 'all_doms'
         self.struc_name = struc_name
-        self.struc_name = 'all_doms'
         self.ner_network = ExtractionNetwork.get('NerV1')(self.ner_name)
         self.template = None
         self.dir_path = self.dir_path.joinpath('strucTemp').joinpath(self.struc_name)
@@ -63,8 +62,40 @@ class CombinedExtractionModel(BaseExtractionModel):
         structure_result = self.template.extract(web_ids, self.category, k=k, with_score=True)
         print("STRUCTURE:", structure_result)
 
-        # wenn bei mehr als 50% ein Overlap, dann nimm komplpett struc-Modell
-        # score von structure-Modell wenn über 10 dann nimm alles von structure
+        EPSILON = 12
 
-        # wenn weniger, nimm immer den Overlap und vom Rest dann das NER-Modell
-        pass
+        combine_result = []
+        for id_result_ner, id_result_struct in zip(ner_result, structure_result):
+            good_matches = 0
+            id_result = {}
+            for attr in id_result_ner:
+                ner_result = id_result_ner[attr]
+                structure_result = id_result_struct[attr]
+                id_result[attr] = []
+                for score, candiate_struc in structure_result:
+                    if not candiate_struc or score < EPSILON:
+                        continue
+                    candiate_struc = text_preprocessing.preprocess_text_html(candiate_struc)
+                    for candidate_ner in ner_result:
+                        if not candidate_ner:
+                            continue
+                        candidate_ner = text_preprocessing.preprocess_text_html(candidate_ner)
+                        if candidate_ner in candiate_struc or candiate_struc in candidate_ner:
+                            good_matches += 1
+                            id_result[attr].append(candiate_struc)
+                            continue
+                if not id_result[attr]:
+                    id_result[attr].append(ner_result)
+
+            if good_matches > len(id_result_ner)/2:
+                new_dict = {}
+                for attr in id_result_struct:
+                    new_dict[attr] = []
+                    for tupel in id_result_struct[attr]:
+                        if tupel[1] and tupel[0] > EPSILON/2:
+                            new_dict[attr].append(tupel[1])
+                combine_result.append(new_dict)
+            else:
+                combine_result.append(id_result)
+
+        return combine_result
--- startup.py
+++ startup.py
@@ -114,13 +114,14 @@ def main():
     # # struc_temp_model.train(train_ids)
     # result = struc_temp_model.extract(web_ids, k=3, n_jobs=-2)
     # print(f'{len(result)}, {len(web_ids)}')
+    '''
+    ner_temp_model = NeuralNetExtractionModel(Category.NBA_PLAYER, 'text_1', 'NerV1')
+    history = ner_temp_model.train(web_ids)
+    result = ner_temp_model.extract(train_ids)
+    print(result)
+    '''
 
-    # ner_temp_model = NeuralNetExtractionModel(Category.NBA_PLAYER, 'text_1', 'NerV1')
-    # history = ner_temp_model.train(web_ids)
-    # result = ner_temp_model.extract(train_ids)
-    # print(result)
-
-    combine_model = CombinedExtractionModel(Category.NBA_PLAYER, 'text_1', 'stuc_1')
+    combine_model = CombinedExtractionModel(Category.NBA_PLAYER, ner_name='text_1', struc_name='all_doms')
     result = combine_model.extract(web_ids[:2])
     print(result)
 
