tobiasschreieder/web-b-gone

commit 2bdcfa6a6fe572207a6dcf62031b9f3ba1633d28

add enum to int, working network
decision to rewrite network because error 'ValueError: slice index 0 of dimension 0 out of bounds. for '{{node strided_slice}} = StridedSlice[Index=DT_INT32, T=DT_INT32, begin_mask=0, ellipsis_mask=0, end_mask=0, new_axis_mask=0, shrink_axis_mask=1](Shape, strided_slice/stack, strided_slice/stack_1, strided_slice/stack_2)' with input shapes: [0], [1], [1], [1] and with computed input tensors: input[1] = <0>, input[2] = <1>, input[3] = <1>.'
was impossible to fix for me.

classification/preprocessing/data_storage.py
@@ -1,7 +1,7 @@
 import dataclasses
 import json
 import random
-from enum import Enum
+from aenum import Enum, MultiValue
 from typing import List, Dict, Union
 
 from config import Config
@@ -11,15 +11,18 @@ faulty_ids = []
 
 
 class Category(Enum):
-    AUTO = 'Auto'
-    BOOK = 'Book'
-    CAMERA = 'Camera'
-    JOB = 'Job'
-    MOVIE = 'Movie'
-    NBA_PLAYER = 'NBA Player'
-    RESTAURANT = 'Restaurant'
-    UNIVERSITY = 'University'
-    NONE = 'NONE'
+    _init_ = 'value fullname'
+    _settings_ = MultiValue
+
+    AUTO = 1, 'Auto'
+    BOOK = 2, 'Book'
+    CAMERA = 3, 'Camera'
+    JOB = 4, 'Job'
+    MOVIE = 5, 'Movie'
+    NBA_PLAYER = 6, 'NBA Player'
+    RESTAURANT = 7, 'Restaurant'
+    UNIVERSITY = 8, 'University'
+    NONE = 9, 'NONE'
 
     @staticmethod
     def get(name: str) -> 'Category':
@@ -36,6 +39,9 @@ class Category(Enum):
             if name_low == 'nba player' or name_low == 'nbaplayer' or name_low == 'nba_player':
                 return Category.NBA_PLAYER
         raise ValueError(f'No Category found with name "{name}"')
+
+    def __int__(self):
+        return self.value
 
     def get_attribute_names(self) -> List[str]:
         """

classification/category_models/category_networks/cat_net_v3.py
@@ -1,0 +1,84 @@
+import random
+from typing import List
+import collections
+import pathlib
+
+import tensorflow_datasets as tfds
+import tensorflow as tf
+import pandas as pd
+import tensorflow as tf
+
+from keras import layers
+from keras import losses
+from keras import layers
+from keras import losses
+from keras.layers import TextVectorization, Dense
+from keras.models import Sequential
+
+
+from sklearn.model_selection import train_test_split
+from tensorflow.python.keras.metrics import binary_accuracy
+
+from .base_category_network import BaseCategoryNetwork
+from ...preprocessing import Category, text_vectorization
+from ...preprocessing.categorize_prepare import get_dict_from_feature_list, create_feature_list, get_true_categorys, \
+    get_all_text_from_feature_list, get_dict_from_feature_list_inkl_keyword_result
+
+
+tfds.disable_progress_bar()
+class CategoryNetworkV3(BaseCategoryNetwork):
+
+    def __init__(self, name: str = "Categorize_text_all", **kwargs):
+        super().__init__(name=name, version='v3', description='Keras complete try.')
+
+    def predict(self, web_ids: List[str]) -> List[Category]:
+        """
+        """
+        self.load()  # self.load() later
+        feature_dict = get_dict_from_feature_list_inkl_keyword_result(create_feature_list(web_ids), web_ids)
+        X = pd.get_dummies(feature_dict.drop(['true_category', 'web_id'], axis=1))
+        # loss, accuracy = self.model.predict(feature_dict)
+        # print("Accuracy: {:2.2%}".format(binary_accuracy))
+        return self.model.predict(X)
+
+
+    def classification(self, web_ids: List[str]) -> List[Category]:
+        return self.predict( web_ids)
+
+
+    def train(self, web_ids: List[str]) -> None:
+        #feature_dict = get_all_text_from_feature_list(create_feature_list(web_ids), web_ids)
+        feature_dict = get_dict_from_feature_list_inkl_keyword_result(create_feature_list(web_ids), web_ids)
+        result_list = get_true_categorys(web_ids)
+        print(feature_dict)
+        X_train = pd.get_dummies(feature_dict.drop(['true_category', 'web_id'], axis=1))
+        y_train = feature_dict['true_category'].apply(lambda x: int(x))
+        model = tf.keras.Sequential([layers.Dense(4)])
+        model = Sequential()
+        model.add(Dense(units=32, activation='relu', input_dim=len(X_train.columns)))
+        model.add(Dense(units=64, activation='relu'))
+        model.add(Dense(units=64, activation='relu'))
+        model.add(Dense(units=9, activation='softmax'))  # softmax
+
+        model.compile(
+            loss=losses.SparseCategoricalCrossentropy(from_logits=True),
+            optimizer='adam',
+            metrics='accuracy')
+        model.fit(X_train, y_train, epochs=24, batch_size=32)
+        self.model = model
+        #self.model = self.build_model(text_vectorization.binary_vectorize_layer, text_vectorization.binary_model)
+        self.save()
+        # (number of inputs + 1 output)/2 = #hiddenLayers
+        # sigmoid for classifikation as activation
+        pass
+
+    def build_model(self, binary_vectorize_layer, binary_model):
+        export_model = tf.keras.Sequential(
+            [binary_vectorize_layer, binary_model,
+             layers.Activation('softmax')])
+
+        export_model.compile(
+            loss=losses.SparseCategoricalCrossentropy(from_logits=False),
+            optimizer='adam',
+            metrics=['accuracy'])
+        return export_model

classification/preprocessing/categorize_prepare.py
@@ -85,10 +85,11 @@ def get_all_text_from_feature_list(feature_list: List[FeatureHolder], web_ids: L
 def get_dict_from_feature_list_inkl_keyword_result(feature_list: List[FeatureHolder], web_ids: List[str]) -> pd.DataFrame:
     train_dict = pd.DataFrame()
     for f in feature_list:
-        df = pd.DataFrame({'web_id': [f.web_id], 'url': [f.url], 'head': [f.head], 'title': [f.title], 'domain_name': [f.domain_name], 'html': [f.html], 'hyperlinks': [f.link], 'text_all': [f.text_all], 'keyword_result': [find_class(f.html)], 'true_category': [f.true_category]}, index=[f.web_id],)
+        #df = pd.DataFrame({'web_id': [f.web_id], 'url': [f.url], 'head': [f.head], 'title': [f.title], 'domain_name': [f.domain_name], 'html': [f.html], 'hyperlinks': [f.link], 'text_all': [f.text_all], 'keyword_result': [find_class(f.html)], 'true_category': [int(f.true_category)]}, index=[f.web_id],)
+        df = pd.DataFrame({'web_id': [f.web_id], 'url': [f.url], 'html': [f.html], 'text_all': [f.text_all], 'true_category': [int(f.true_category)]}, index=[f.web_id],)
         #df["true_category"] = df["true_category"].astype("category")
         train_dict = pd.concat([train_dict, df])
-        train_dict["true_category"] = pd.Categorical(
-            train_dict["true_category"], categories=[Category.AUTO,Category.BOOK,Category.CAMERA,Category.JOB,Category.MOVIE,Category.NBA_PLAYER,Category.RESTAURANT,Category.UNIVERSITY], ordered=False
-        )
+        #train_dict["true_category"] = pd.Categorical(
+        #    train_dict["true_category"], categories=[1,2,3,4,5,6,7,8,9], ordered=False
+        #)
     return train_dict

commit 24486fc2aabc557a8390b67426edbe2202e186c9

reworked Category, cause Commit 2bdcfa6a6fe572207a6dcf62031b9f3ba1633d28 broke it.

classification/preprocessing/data_storage.py
@@ -1,7 +1,7 @@
 import dataclasses
 import json
 import random
-from aenum import Enum, MultiValue
+from enum import Enum
 from typing import List, Dict, Union
 
 from config import Config
@@ -11,37 +11,42 @@ faulty_ids = []
 
 
 class Category(Enum):
-    _init_ = 'value fullname'
-    _settings_ = MultiValue
-
-    AUTO = 0, 'Auto'
-    BOOK = 1, 'Book'
-    CAMERA = 2, 'Camera'
-    JOB = 3, 'Job'
-    MOVIE = 4, 'Movie'
-    NBA_PLAYER = 5, 'NBA Player'
-    RESTAURANT = 6, 'Restaurant'
-    UNIVERSITY = 7, 'University'
-    NONE = 8, 'NONE'
+    AUTO = 'Auto'
+    BOOK = 'Book'
+    CAMERA = 'Camera'
+    JOB = 'Job'
+    MOVIE = 'Movie'
+    NBA_PLAYER = 'NBA Player'
+    RESTAURANT = 'Restaurant'
+    UNIVERSITY = 'University'
+    NONE = 'NONE'
 
     @staticmethod
-    def get(name: str) -> 'Category':
+    def get(query: Union[str, int]) -> 'Category':
         """
         TODO
-        :param name:
+        :param query:
         :return:
         """
-        name_low = name.lower()
-        for cat in Category:
-            if name_low == cat.name.lower():
-                return cat
-        else:
-            if name_low == 'nba player' or name_low == 'nbaplayer' or name_low == 'nba_player':
-                return Category.NBA_PLAYER
-        raise ValueError(f'No Category found with name "{name}"')
+        if isinstance(query, str):
+            name_low = query.lower()
+            for cat in Category:
+                if name_low == cat.name.lower():
+                    return cat
+            else:
+                if name_low == 'nba player' or name_low == 'nbaplayer' or name_low == 'nba_player':
+                    return Category.NBA_PLAYER
+            raise ValueError(f'No Category found with name "{query}"')
+        elif isinstance(query, int):
+            all_cat = [cat for cat in Category]
+            if not query < len(all_cat):
+                raise ValueError(f'No Category found with number "{query}"')
+            return all_cat[query]
+
+        raise ValueError(f'No Category found with query "{query}"')
 
     def __int__(self):
-        return self.value
+        return [cat for cat in Category].index(self)
 
     def get_attribute_names(self) -> List[str]:
         """